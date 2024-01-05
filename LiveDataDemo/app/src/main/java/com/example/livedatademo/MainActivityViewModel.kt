6fabf0e9d4c10bbd0b54cc285d3d752d27dc53e9
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-01-05 09:37:57 +0300

    Update

---
+++ LiveDataDemo/app/src/main/java/com/example/livedatademo/MainActivityViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.livedatademo
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.example.livedatademo.model.User
+import com.example.livedatademo.model.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivityViewModel : ViewModel()  {
+
+    private var usersRepository= UserRepository()
+
+    var users = liveData(Dispatchers.IO) {
+        val result = usersRepository.getUsers()
+        emit(result)
+    }
+
+//    private var usersRepository= UserRepository()
+//    var users: MutableLiveData<List<User>?> = MutableLiveData()
+//
+//    fun getUsers() {
+//        viewModelScope.launch {
+//            var result: List<User>? = null
+//            withContext(Dispatchers.IO) {
+//                result = usersRepository.getUsers()
+//            }
+//            users.value = result
+//        }
+//    }
+}
