cf05a877ee374d5e531f23a6fbb4a60949687d81
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-02-02 21:20:11 +0300

    Update

--- ComposeStateDemo/app/src/main/java/com/example/composestatedemo/MainActivity.kt
+++ ComposeStateDemo/app/src/main/java/com/example/composestatedemo/MainActivity.kt
@@ -5,50 +5,71 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.composestatedemo.ui.theme.ComposeStateDemoTheme
 
+// MainActivity class, extending ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Set the content of the activity using Compose
         setContent {
+            val viewModel = viewModel<MyViewModel>()
+            // Apply the ComposeStateDemoTheme to the content
             ComposeStateDemoTheme {
+                // Column composable that aligns its children in a vertical arrangement at the center
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    MyButton()
+                    // Call the MyButton composable within the Column
+                    // Mutable state representing the count
+                    var count   = viewModel.count
+                    MyButton(count)
+                    { count = it + 1 }
                 }
             }
         }
     }
 }
 
-val count = mutableIntStateOf(0)
 
-@Preview(name = "MyButton")
 @Composable
-fun MyButton(){
+fun MyButton(currentCount: Int, updateCount:(Int)->Unit) {
+    // Get the current context
     val context = LocalContext.current
-    Button(onClick = {
-        count.value = count.value+1
-        Toast.makeText(context, "Count is : ${count.value}", Toast.LENGTH_SHORT).show()
-    },
+    // Button composable with onClick listener
+
+
+    Button(
+        onClick = {
+            // Increment the count when the button is clicked
+            updateCount(currentCount)
+        },
+        // Button styling properties
         contentPadding = PaddingValues(16.dp),
         border = BorderStroke(10.dp, Color.Black),
         colors = ButtonDefaults.textButtonColors(
@@ -56,10 +77,12 @@ fun MyButton(){
             contentColor = Color.White
         )
     ) {
-        Text("Count is : ${count.value}",
+        // Text composable displaying the current count value
+        Text(
+            "Count is : $currentCount",
+            // Styling using MaterialTheme's headlineMedium
             style = MaterialTheme.typography.headlineMedium,
             modifier = Modifier.padding(5.dp)
         )
     }
 }
-
