77c3dd401f031e2405347bfb882a5272a15867f9
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-02-02 09:06:56 +0300

    Update

--- JetPackCompose/SnackBar/app/src/main/java/com/android/snackbar/MainActivity.kt
+++ JetPackCompose/SnackBar/app/src/main/java/com/android/snackbar/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
