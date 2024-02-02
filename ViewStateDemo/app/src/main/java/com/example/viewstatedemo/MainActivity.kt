a677bea4d801da9107574ecb2780678a554834a4
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-02-02 15:56:46 +0300

    Update

---
+++ ViewStateDemo/app/src/main/java/com/example/viewstatedemo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.viewstatedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewButton()
+    }
+
+    private fun viewButton() {
+        val button = findViewById<Button>(R.id.btnCount)
+        button.text = "Count is : $count"
+
+        button.setOnClickListener {
+            button.text = "Count is : ${++count}"
+            Toast.makeText(
+                this, "Count is : $count",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+
+}
