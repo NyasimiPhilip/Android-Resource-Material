fefa6c5d98a778483d78b099699e8cb7b08c897f
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-02-03 10:42:53 +0300

    Update

--- JetPackCompose/FlowDemo1/app/build.gradle.kts
+++ JetPackCompose/FlowDemo1/app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- JetPackCompose/FlowDemo1/app/src/main/java/com/android/flowdemo1/MainActivity.kt
+++ JetPackCompose/FlowDemo1/app/src/main/java/com/android/flowdemo1/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.android.flowdemo1
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val myFlow = flow<Int>{
+            for (i in 1..100){// In the context of flows this is the producer
+                emit(i)
+                delay(1000L)
+            }
 
-
+        }
+        val textView = findViewById<TextView>(R.id.tvResult)
+        CoroutineScope(Dispatchers.Main).launch{
+            myFlow.collectLatest {
+                textView.text = "Current Index is $it"
+            }
+        }
     }
 }
 
