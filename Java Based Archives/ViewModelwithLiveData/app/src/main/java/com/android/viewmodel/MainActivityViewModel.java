aa5f81e6ec34b9d4df299e038ecd8bca83294af8
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-02-11 08:07:07 +0300

    Update README.md

---
+++ Java Based Archives/ViewModelwithLiveData/app/src/main/java/com/android/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,15 @@
+package com.android.viewmodel;
+
+import androidx.lifecycle.ViewModel;
+public class MainActivityViewModel extends ViewModel {
+    private int clickCount = 0;
+    public int getInitialCount() {
+
+        return clickCount;
+    }
+    public int getCurrentCount() {
+
+        clickCount += 1;
+        return clickCount;
+    }
+}
