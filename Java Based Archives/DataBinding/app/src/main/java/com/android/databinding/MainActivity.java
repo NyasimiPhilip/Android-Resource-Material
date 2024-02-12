0c25936523356f595a82cbbdfc4678625881c6b1
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-02-12 12:19:11 +0300

    Update

--- Java Based Archives/DataBinding/app/src/main/java/com/android/databinding/MainActivity.java
+++ Java Based Archives/DataBinding/app/src/main/java/com/android/databinding/MainActivity.java
@@ -3,17 +3,21 @@ package com.android.databinding;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-  /*   private TextView nameText;
+  /* private TextView nameText;
     private TextView emailText;
   */
     private ActivityMainBinding activityMainBinding;
+    private MainActivityClickHandlers handlers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         activityMainBinding.setStudent(getCurrentStudent());
 
+        handlers = new MainActivityClickHandlers(this);
+        activityMainBinding.setClickHandler(handlers);
+
 
     }
 
@@ -38,5 +45,17 @@ public class MainActivity extends AppCompatActivity {
         student.setStudentEmail("alex@gmail.com");
         return student;
 
+    }
+    public class MainActivityClickHandlers{
+        Context context;
+        public MainActivityClickHandlers(Context context){
+            this.context = context;
+        }
+        public void onEnrollButtonClicked(View view){
+            Toast.makeText(context, "Enroll Clicked", Toast.LENGTH_LONG).show();
+        }
+        public void onCancelButtonClicked(View view){
+            Toast.makeText(context, "Cancel Clicked", Toast.LENGTH_LONG).show();
+        }
     }
 }
