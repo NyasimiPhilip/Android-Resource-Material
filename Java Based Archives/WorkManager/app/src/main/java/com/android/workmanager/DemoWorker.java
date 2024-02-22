c06cf957d2b6ce1026d3d125fc0f721756d33e4b
NyasimiPhilip <nyasimiphilip@gmail.com>
2024-02-22 12:48:19 +0300

    Update

--- Java Based Archives/WorkManager/app/src/main/java/com/android/workmanager/DemoWorker.java
+++ Java Based Archives/WorkManager/app/src/main/java/com/android/workmanager/DemoWorker.java
@@ -4,20 +4,41 @@ import android.content.Context;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+
 public class DemoWorker extends Worker {
+
+
+    public static final String KEY_WORKER = "key_worker";
+
+    // Constructor for DemoWorker class.
     public DemoWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
 
+
     @NonNull
     @Override
     public Result doWork() {
-        for(int i = 0;i < 10000; i++)
+        // Retrieve input data from the Data object.
+        Data data = getInputData();
 
-            Log.i("MYTAG","Count is "+i);
-        return Result.success();
+        // Get the integer value associated with the key KEY_COUNT_VALUE from input data.
+        int countList = data.getInt(MainActivity.KEY_COUNT_VALUE, 0);
+
+        // Loop through countList and log each count.
+        for(int i = 0; i < countList; i++)
+            Log.i("MYTAG", "Count is " + i);
+
+        // Create output data to be sent back.
+        Data dataToSend = new Data.Builder()
+                .putString(KEY_WORKER, "Task Done Successfully")
+                .build();
+
+        // Return a successful result with output data.
+        return Result.success(dataToSend);
     }
 }
--- Java Based Archives/WorkManager/app/src/main/java/com/android/workmanager/MainActivity.java
+++ Java Based Archives/WorkManager/app/src/main/java/com/android/workmanager/MainActivity.java
@@ -1,54 +1,72 @@
 package com.android.workmanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintSet;
-import androidx.lifecycle.Observer;
-import androidx.work.Constraints;
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.WorkInfo;
-import androidx.work.WorkManager;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView textView;
+    public static final String KEY_COUNT_VALUE = "key count value";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Set the layout for the activity.
+
+        // Create a Data object to hold key-value pairs.
+        Data data = new Data.Builder().putInt(KEY_COUNT_VALUE, 4500).build();
+
+        // Create Constraints for the work request.
         Constraints constraint = new Constraints.Builder()
                 .setRequiresCharging(true)
                 .build();
 
-
+        // Create a OneTimeWorkRequest for the DemoWorker class with input data and constraints.
         final OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(DemoWorker.class)
-                .setConstraints(constraint)
+                //.setConstraints(constraint)
+                .setInputData(data)
                 .build();
 
-
-
-        button= findViewById(R.id.button);
+        // Find and initialize the Button and TextView from the layout.
+        button = findViewById(R.id.button);
         textView = findViewById(R.id.tvStatus);
+
+        // Set a click listener for the Button.
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Enqueue the work request to the WorkManager when the Button is clicked.
                 WorkManager.getInstance().enqueue(oneTimeWorkRequest);
             }
         });
+
+        // Observe the WorkInfo associated with the oneTimeWorkRequest.
         WorkManager.getInstance().getWorkInfoByIdLiveData(oneTimeWorkRequest.getId())
                 .observe(this, new Observer<WorkInfo>() {
                     @Override
                     public void onChanged(WorkInfo workInfo) {
-                        if(workInfo!= null)
-                        {
-                        textView.setText(workInfo.getState().name());
+                        if (workInfo != null) {
+                            // Update the TextView with the current state of the work request.
+                            textView.setText(workInfo.getState().name());
+                        }
+                        if (workInfo.getState().isFinished()) {
+                            // If the work request is finished, retrieve output data and display a Toast message.
+                            Data data1 = workInfo.getOutputData();
+                            String message = data1.getString(DemoWorker.KEY_WORKER);
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
                         }
                     }
                 });
-
     }
 }
